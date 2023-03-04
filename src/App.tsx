e093a6db63d897cdf0c5efb5f625efd15fa46ddc
Brock Kennedy <brock97k@gmail.com>
2023-03-04 16:15:34 -0400

    Changed of input

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,30 @@ function App() {
   return (
     <div className="flex flex-col items-center animate-fade-in-down h-screen justify-center relative cursor-default">
       <div className="max-w-md mx-auto">
-        <div className="relative flex items-center w-full h-12 rounded-lg shadow-lg bg-white overflow-hidden">
-          <div className="grid place-items-center h-full w-12 text-gray-300">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+        <div className="relative group flex items-center w-full h-12 rounded-full outline outline-1 outline-black bg-amber-50 hover:bg-black overflow-hidden">
+          <div className="grid place-items-center h-full w-12">
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 stroke-black group-hover:stroke-amber-50 hover:stroke-amber-600"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
           </div>
 
           <input
-            className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+            className="peer bg-inherit h-full w-full outline-none text-sm text-black placeholder-gray-400 group-hover:placeholder-amber-50 group-hover:text-amber-50 pr-2"
             type="text"
             id="search"
-            placeholder="Search something.."
+            placeholder="Search something..."
             autoFocus
           />
         </div>
