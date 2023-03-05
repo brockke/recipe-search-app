0ed615c6706187a08e10dda0f5bd766253964a57
Brock Kennedy <brock97k@gmail.com>
2023-03-05 01:54:15 -0400

    Query API and show results

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
 import "./App.css";
 
+import { useQuery } from "@tanstack/react-query";
+import {
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactFragment,
+  ReactPortal,
+  useState,
+} from "react";
+// import { env } from "process";
+
 function App() {
+  const [queryString, setQueryString] = useState("");
+  const [page, setPage] = useState(0);
+  const [cuisine, setCusine] = useState("");
+
+  const fetchFood = async (page = 0, queryString: string, cuisine: string) => {
+    const NUM_RETURN = 2;
+    return await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${queryString}&number=${NUM_RETURN}&offset=${
+        page * NUM_RETURN
+      }&apiKey=${
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        import.meta.env.VITE_SPOONACULAR_API_KEY
+      }`,
+    ).then((res) => res.json());
+  };
+
+  const { isLoading, isError, data, isFetching, isPreviousData } = useQuery({
+    queryKey: ["projects", page],
+    queryFn: () => fetchFood(page, queryString, cuisine),
+    keepPreviousData: true,
+    refetchInterval: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(data?.results);
   return (
     <div className="flex flex-col items-center animate-fade-in-down h-screen justify-center relative cursor-default">
       <div className="max-w-md mx-auto">
         <div className="relative group flex items-center w-full h-12 rounded-full outline outline-1 outline-black bg-amber-50 hover:bg-black overflow-hidden">
           <div className="grid place-items-center h-full w-12">
-            <button>
+            <button
+              onClick={() => {
+                setQueryString("TEST");
+              }}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 stroke-black group-hover:stroke-amber-50 hover:stroke-amber-600"
@@ -31,6 +72,48 @@ function App() {
             autoFocus
           />
         </div>
+      </div>
+      <div>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : isError ? (
+          <div>Error</div>
+        ) : (
+          <div>
+            {data.results.map(
+              (food: {
+                id: number;
+                image: string;
+                imageType: string;
+                title: string;
+              }) => (
+                <div key={food.id}>
+                  <p>{food.title}</p>
+                  <h1> {food.image} </h1>
+                </div>
+              ),
+            )}
+          </div>
+        )}
+        <span>Current Page: {page + 1}</span>
+        <button
+          onClick={() => setPage((old) => Math.max(old - 1, 0))}
+          disabled={page === 0}
+        >
+          Previous Page
+        </button>{" "}
+        <button
+          onClick={() => {
+            if (!isPreviousData && data.hasMore) {
+              setPage((old) => old + 1);
+            }
+          }}
+          // Disable the Next Page button until we know a next page is available
+          disabled={isPreviousData || !data?.hasMore}
+        >
+          Next Page
+        </button>
+        {isFetching ? <span> Loading...</span> : null}{" "}
       </div>
     </div>
   );
